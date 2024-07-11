8e4bc17eb5b77ae9043d95b1791ca2a4fed198e7
K4NEI <kimyoungho0415@gmail.com>
2024-07-11 16:39:21 +0900

    feat/black: upated

---
+++ specscore-app/src/components/Search/Search.tsx
@@ -0,0 +1,11 @@
+import CHeader from '@components/common/CHeader/CHeader';
+
+const Search = () => {
+  return (
+    <section className='flex flex-col'>
+      <CHeader label='강의 검색' prev='/' />
+    </section>
+  );
+};
+
+export default Search;
---
+++ specscore-app/src/components/Search/containers/SearchContainer.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import Search from '../Search';
+
+const SearchContainer = () => {
+  return <Search />;
+};
+
+export default SearchContainer;
